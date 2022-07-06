b46d013c0fd7cb5930a3abf977a742954b46b229
ahao <1140329214@qq.com>
2022-07-06 15:27:01 +0800

    ljh——0706 将txt文件内容抽取保存为csv文件

--- ChineseCscwProject/dataprocess.py
+++ ChineseCscwProject/dataprocess.py
@@ -1,14 +1,24 @@
 import os
+from pandas.core.frame import DataFrame
 
-data_dir = r'C:\Users\ljh\Desktop\-\cscw数据竞赛\SCHOLAT Interactive User Recommendation\user_attribute'
+
+data_dir = r'.\user_attribute'
+
+
+def dataProcess(data):
+    return '0'
+
 
 data_document_name = os.listdir(data_dir)
 res = []
-for name in data_document_name[:5]:
+for name in data_document_name:
     datapath = data_dir + '\\' + name
 
     with open(datapath, 'r', encoding="utf_8") as f:
         data = f.readline()
-        data.rstrip('')
+        data = data.rstrip(' ')
+        # data = dataProcess(data)
         li = data.split(' ')
-        print(li)
+        res.append(li)
+d = DataFrame(res)
+d.to_csv('data.csv')
